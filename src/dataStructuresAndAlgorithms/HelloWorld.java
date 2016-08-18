c2d24c1d790308005cc04c2d003c18fb5048b0cc
patrickjh <patrick@lessonswithpatrick.com>
2016-08-17 23:36:20 -0700

    Set up ordered array class. No tests yet.

--- src/dataStructuresAndAlgorithms/HelloWorld.java
+++ src/dataStructuresAndAlgorithms/HelloWorld.java
@@ -1,6 +1,6 @@
-package DataStructuresAndAlgorithms;
+package dataStructuresAndAlgorithms;
 
-public class helloWorld {
+public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ src/dataStructuresAndAlgorithms/chapter2Arrays/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author patrick
+ *
+ */
+package dataStructuresAndAlgorithms.chapter2Arrays;
